dcf308ac7d45d1fdef4b233504b591ba853cc592
Hacker <minubgp@gmail.com>
2026-04-02 10:02:53 +0530

    Update main.cs

--- main.cs
+++ main.cs
@@ -1,1 +1,10 @@
-Console.WriteLine("Hello World")
+
+using System;
+
+Console.WriteLine("What is your name?");
+string name = Console.ReadLine();
+
+Console.WriteLine($"Hello, {name}! Today is {DateTime.Now:dddd}.");
+
+void SayGoodbye() => Console.WriteLine("Goodbye!");
+SayGoodbye();
